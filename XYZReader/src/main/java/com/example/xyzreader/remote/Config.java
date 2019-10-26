e3fec4745a9ca30211a65c84eff01f71c46cef16
Karl Arthur <karl.crispen.arthur@gmail.com>
2019-10-26 15:24:58 -0600

    feat: Use smaller articles with different aspect ratios

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -8,11 +8,13 @@ import java.net.URL;
 public class Config {
     public static final URL BASE_URL;
     private static String TAG = Config.class.toString();
+//    private static final String REMOTE_URL = "https://go.udacity.com/xyz-reader-json";
+    private static final String REMOTE_URL = "https://raw.githubusercontent.com/SuperAwesomeness/XYZReader/master/data.json";
 
     static {
         URL url = null;
         try {
-            url = new URL("https://go.udacity.com/xyz-reader-json" );
+            url = new URL(REMOTE_URL);
         } catch (MalformedURLException ignored) {
             // TODO: throw a real error
             Log.e(TAG, "Please check your internet connection.");
